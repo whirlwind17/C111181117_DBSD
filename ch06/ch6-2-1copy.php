9be9cf6c38a2def25cdebb92093b9a2a835aea02
whirlwind17 <whirlwind13220@gmail.com>
2024-11-21 10:02:03 +0800

    1121

--- ch06/ch6-2-1copy.php
+++ ch06/ch6-2-1copy.php
@@ -1,23 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8" />
-<title>ch6-2-1.php</title>
-<?php 
+<?php
 function printHeader() {
    print "<h3>PHP與MySQL網頁設計</h3>";
    echo "<hr/>";
-} 
+}
 function printFooter() {
    print "<hr/>(c)Copyright by 陳會安<br/>";
-} 
+}
 ?>
-</head>
-<body>
-<?php 
-printHeader(); 
+
+<?php
+printHeader();
 print "<p>在PHP程式使用函數建立標題和註腳文字</p>";
-printFooter();  
+printFooter();
 ?>
-</body>
-</html>
