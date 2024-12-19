1f8a636e0fa4384197e422c345fd361df5c956d6
whirlwind17 <whirlwind13220@gmail.com>
2024-12-19 11:17:55 +0800

    8-4-3,3-5-2

---
+++ ch08/ch8-4-3copy.php
@@ -0,0 +1,10 @@
+<?php
+$username = $_POST["User"];
+$password = $_POST["Pass"];
+print "姓名: ".$username."<br/>";
+print "密碼: ".$password."<br/>";
+$address = $_POST["Address"];
+print "地址: <br/>".nl2br($address)."<br/>";
+$type = $_POST["Type"];
+print "種類: ".$type."<br/>";
+?>
