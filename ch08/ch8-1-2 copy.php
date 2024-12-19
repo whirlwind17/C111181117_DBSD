76e16f45b672e627db6ebb1c53452000ebe72b22
whirlwind17 <whirlwind13220@gmail.com>
2024-12-19 11:41:11 +0800

    8-1-2

--- ch08/ch8-1-2 copy.php
+++ ch08/ch8-1-2 copy.php
@@ -1,16 +1,12 @@
 <?php 
 $ip = $_SERVER["REMOTE_ADDR"];
-
 print 'Ip Address' . ''. $ip .'';
-
 ?>
-
-
 <table border="1">
-   <tr>
-      <td>1</td>
-      <td>2</td>
-   </tr>   
+   <Tr>
+      <td>C111181117</td>
+      <td>Alan</td>
+   </Tr>   
    
    <?php
    foreach($_SERVER as $key => $value) {
